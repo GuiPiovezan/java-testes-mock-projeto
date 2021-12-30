a0ac0be6d5d69e1016535ec6a5d6e6d7a3818d16
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-12-30 08:42:11 -0300

    Adicionando um novo teste para simular uma exceção ao salvar um leilão, portanto o comportamento de enviar e-mail não pode ser executado

--- src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
+++ src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -18,79 +18,96 @@ import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
 
 class FinalizarLeilaoServiceTest {
-	
-	private FinalizarLeilaoService service;
-	
-	@Mock
-	private LeilaoDao leilaoDao;
 
-	@Mock
-	private EnviadorDeEmails enviadorDeEmails;
-	
-	@BeforeEach
-	public void beforeEach() {
-		MockitoAnnotations.initMocks(this);
-		this.service = new FinalizarLeilaoService(leilaoDao, enviadorDeEmails);
-	}
+    private FinalizarLeilaoService service;
 
-	@Test
-	void deveriaFinalizarUmLeilao() {
-		List<Leilao> leiloes = leiloes();
+    @Mock
+    private LeilaoDao leilaoDao;
+
+    @Mock
+    private EnviadorDeEmails enviadorDeEmails;
+
+    @BeforeEach
+    public void beforeEach() {
+        MockitoAnnotations.initMocks(this);
+        this.service = new FinalizarLeilaoService(leilaoDao, enviadorDeEmails);
+    }
+
+    @Test
+    void deveriaFinalizarUmLeilao() {
+        List<Leilao> leiloes = leiloes();
 
 
         // Ao chamar tal método ele irá retornar uma lista dele leilões
-		Mockito.when(leilaoDao.buscarLeiloesExpirados())
-		.thenReturn(leiloes);
-		
-		service.finalizarLeiloesExpirados();
-		
-		Leilao leilao = leiloes.get(0);
-		Assertions.assertTrue(leilao.isFechado());
-		Assertions.assertEquals(new BigDecimal("900"), leilao.getLanceVencedor().getValor());
+        Mockito.when(leilaoDao.buscarLeiloesExpirados())
+                .thenReturn(leiloes);
 
-		//Verifica se determinado método foi executado
-		Mockito.verify(leilaoDao).salvar(leilao);
-	}
+        service.finalizarLeiloesExpirados();
 
-	@Test
-	void deveriaEnviarUmEmailAoVencedorDoLeilao() {
-		List<Leilao> leiloes = leiloes();
+        Leilao leilao = leiloes.get(0);
+        Assertions.assertTrue(leilao.isFechado());
+        Assertions.assertEquals(new BigDecimal("900"), leilao.getLanceVencedor().getValor());
+
+        //Verifica se determinado método foi executado
+        Mockito.verify(leilaoDao).salvar(leilao);
+    }
+
+    @Test
+    void deveriaEnviarUmEmailAoVencedorDoLeilao() {
+        List<Leilao> leiloes = leiloes();
 
 
-		// Ao chamar tal método ele irá retornar uma lista dele leilões
-		Mockito.when(leilaoDao.buscarLeiloesExpirados())
-				.thenReturn(leiloes);
+        // Ao chamar tal método ele irá retornar uma lista dele leilões
+        Mockito.when(leilaoDao.buscarLeiloesExpirados())
+                .thenReturn(leiloes);
 
-		service.finalizarLeiloesExpirados();
+        service.finalizarLeiloesExpirados();
 
-		Leilao leilao = leiloes.get(0);
+        Leilao leilao = leiloes.get(0);
 
-		Lance lanceVencedor = leilao.getLanceVencedor();
+        Lance lanceVencedor = leilao.getLanceVencedor();
 
-		//Verifica se determinado método foi executado
-		Mockito.verify(enviadorDeEmails).enviarEmailVencedorLeilao(lanceVencedor);
-	}
+        //Verifica se determinado método foi executado
+        Mockito.verify(enviadorDeEmails).enviarEmailVencedorLeilao(lanceVencedor);
+    }
+
+    @Test
+    void naoDeveriaEnviarEmailAoVencedorEmCasoDeErroAoFinalizarLeilao() {
+        List<Leilao> leiloes = leiloes();
+
+        //Vamos simular uma exceção ao salvar um leilão
+        Mockito.when(leilaoDao.salvar(Mockito.any()))
+                .thenThrow(RuntimeException.class);
+
+        try {
+            service.finalizarLeiloesExpirados();
+            //Verifica se determinado método não teve interações
+            Mockito.verifyNoInteractions(enviadorDeEmails);
+        } catch (Exception ignored) {
+            // Não faz nada
+        }
+    }
 
 
     // Criando e iniciando alguns leilões
-	private List<Leilao> leiloes() {
-		List<Leilao> lista = new ArrayList<>();
-		
-		Leilao leilao = new Leilao("Celular", 
-				new BigDecimal("500"), 
-				new Usuario("Fulano"));
-		
-		Lance primeiro = new Lance(new Usuario("Beltrano"), 
-				new BigDecimal("600"));
-		Lance segundo = new Lance(new Usuario("Ciclano"), 
-				new BigDecimal("900"));
-		
-		leilao.propoe(primeiro);
-		leilao.propoe(segundo);
-		
-		lista.add(leilao);
-		
-		return lista;
-	}
+    private List<Leilao> leiloes() {
+        List<Leilao> lista = new ArrayList<>();
+
+        Leilao leilao = new Leilao("Celular",
+                new BigDecimal("500"),
+                new Usuario("Fulano"));
+
+        Lance primeiro = new Lance(new Usuario("Beltrano"),
+                new BigDecimal("600"));
+        Lance segundo = new Lance(new Usuario("Ciclano"),
+                new BigDecimal("900"));
+
+        leilao.propoe(primeiro);
+        leilao.propoe(segundo);
+
+        lista.add(leilao);
+
+        return lista;
+    }
 
 }
